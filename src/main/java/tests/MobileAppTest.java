9b58bcfda7ba87459d5f93e6d032feff3dad43c2
Dmitriy Martynenko <dm.martyn1311@gmail.com>
2019-08-23 12:08:05 +0300

    edit tests

--- src/main/java/tests/MobileAppTest.java
+++ src/main/java/tests/MobileAppTest.java
@@ -4,32 +4,31 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import pages.CalculatorPage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MobileAppTest extends BaseClass {
 
     private CalculatorPage calculatorPage = new CalculatorPage();
 
-    @Test
-    public void testMethod() {
-        $(By.id("digit_2")).click();
-        $(By.id("op_add")).click();
-        $(By.id("digit_4")).click();
-        $(By.id("eq")).click();
-        $(By.id("result")).shouldHave(text("6"));
-        System.out.println("Да, бля!");
-    }
-
-    @Test
-    public void testMethod2() {
-        calculatorPage.pressButton("2").
-                pressPlus().
-                pressButton("4").
-                pressEquals().
-                checkResult("6");
-    }
+//    @Test
+//    public void testMethod() {
+//        $(By.id("digit_2")).click();
+//        $(By.id("op_add")).click();
+//        $(By.id("digit_4")).click();
+//        $(By.id("eq")).click();
+//        $(By.id("result")).shouldHave(text("6"));
+//        System.out.println("Да, бля!");
+//    }
+//
+//    @Test
+//    public void testMethod2() {
+//        calculatorPage.pressButton("2").
+//                pressPlus().
+//                pressButton("4").
+//                pressEquals().
+//                checkResult("6");
+//    }
 
     @Test
     public void testTourist() {
@@ -53,7 +52,7 @@ public class MobileAppTest extends BaseClass {
 
     @Test
     public void setValues() {
-//        $(By.id("progressBar")).waitUntil(not(visible), 30* 1000);
+        $(By.id("progressBar")).waitUntil(not(visible), 30* 1000);
 //        $(By.id("progressBar")).shouldNotBe(visible);
         $(By.id("places_toolbar")).shouldBe(visible).click();
         $(By.id("dynamic_item_container")).shouldBe(visible).val("Кафе");
